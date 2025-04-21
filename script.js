ddee166f65fb0809a395976f52240c81dabb096f
Bharadwaja <91343366+Bharadwaja1557@users.noreply.github.com>
2025-04-21 18:12:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -121,6 +121,7 @@ const navLinks = document.getElementById("nav-links");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
+  hamburger.classList.toggle("active");
 });
 
 initDots();
